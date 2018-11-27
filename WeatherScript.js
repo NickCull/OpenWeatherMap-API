520806685f77de348e2ca2e50da0b55e4b6e24fb
NickCull <44659764+NickCull@users.noreply.github.com>
2018-11-27 10:13:44 -0500

    Update WeatherScript.js

--- WeatherScript.js
+++ WeatherScript.js
@@ -3,11 +3,11 @@ var city = "Boston"
 $.getJSON(
     "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=55454d6789810371ea394c209500de03"),
     Function(data) {
-        //what is wrong here? "console:log(data);" expected
+    
             console.log(data);
 
             var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-            //description pulls full weather description rather than 
+            //.description pulls full weather description rather than 
             var weather = data.weather[0].description;
             var temp = Math.floor(data.main.temp);
 
