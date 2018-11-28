fc4c507b7c2d0cda1eadd0030fa6ff264af29853
NickCull <44659764+NickCull@users.noreply.github.com>
2018-11-27 21:23:27 -0500

    Update WeatherScript.js

--- WeatherScript.js
+++ WeatherScript.js
@@ -1,4 +1,4 @@
-var city = "Boston"
+var city = "austin"
 
 $.getJSON(
     "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=55454d6789810371ea394c209500de03",
@@ -10,10 +10,12 @@ $.getJSON(
             //.description pulls full weather description rather than .main data
             var weather = data.weather[0].description;
             var temp = Math.floor(data.main.temp);
+            var name = data.name;
 
             $('.icon').attr("src", icon);
             $('.weather').append(weather);
             $('.temp').append(temp);
+            $('.name').append(name);
 
     }
 );
